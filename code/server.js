1232fd2cc4fdcae68b943405773621afb3cd54ff
Paul Taele <ptaele@gmail.com>
2018-07-23 14:33:43 -0500

    removed nodejs files

--- code/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var server = express();
-
-server.use(express.static('public'));
-
-server.listen(3000, function() {
-	console.log('listening to port 3000...');
-});
