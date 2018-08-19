8d82eac47607d1c184ab77a3299dd881d31d1f22
Paul Taele <ptaele@gmail.com>
2018-08-19 05:05:30 -0500

    Added first and last name input for sign up page.

--- signup.php
+++ signup.php
@@ -16,53 +16,63 @@
 
     <h1>Kanji Workbook</h1>
 
-      <form id="login_form">
+    <form id="login_form">
 
-        <fieldset id="fieldset_area">
-          <!-- legend -->
-          <legend>Sign Up Details</legend>
+      <fieldset id="fieldset_area">
+        <!-- legend -->
+        <legend>Sign Up Details</legend>
 
-          <!-- username input -->
-          <!-- <label for="username_input">Enter Username:<sup>*</sup></label>
-          <input type="text" id="username_input" name="username_input" required> -->
+        <!-- generate username submit and input -->
+        <label id="username_label" for="username_input" class="generate_label">Generate Username:<sup>*</sup></label>
+        <input type="text" id="username_input" class="generate_input" name="username_input" readonly><br>
+        <input type="button" id="username_button" class="generate_button" value="Generate" onclick="generateUsername();">
 
-          <!-- generate username submit and input -->
-          <label id="username_label" for="username_input" class="generate_label">Generate Username:<sup>*</sup></label><br>
-          <input type="text" id="username_input" class="generate_input" name="username_input" readonly><br>
-          <input type="button" id="username_button" class="generate_button" value="Generate" onclick="generateUsername();">
+        <br><br>
 
-          <br><br>
+        <!-- generate password submit and input -->
+        <label id="password_label" for="password_input" class="generate_label">Generate Password:<sup>*</sup></label>
+        <input type="text" id="password_input" class="generate_input" name="password_input" readonly><br>
+        <input type="button" id="password_button" class="generate_button" value="Generate" onclick="generatePassword();">
 
-          <!-- generate password submit and input -->
-          <label id="password_label" for="password_input" class="generate_label">Generate Password:<sup>*</sup></label><br>
-          <input type="text" id="password_input" class="generate_input" name="password_input" readonly><br>
-          <input type="button" id="password_button" class="generate_button" value="Generate" onclick="generatePassword();">
+        <br><br>
 
-          <br><br>
+        <!-- first name input -->
+        <label for="firstname_input">Enter First Name:<sup>*</sup></label>
+        <input type="text" id="firstname_input" name="firstname_input" required>
 
-          <!-- invite code input -->
-          <label for="code_input">Enter Invite Code:</label>
-          <input type="password" id="code_input" name="code_input">
+        <br><br>
 
-          <br><br>
+        <!-- last name input -->
+        <label for="lastname_input">Enter Last Name:<sup>*</sup></label>
+        <input type="text" id="lastname_input" name="lastname_input" required>
 
-          <!-- challenge input -->
-          <label id="challenge_label" for="challenge_input">Challenge:
-            <span style="font-weight: normal;">What is the capital of Japan?:</span><sup>*</sup>
-          </label><br>
-          <input type="text" id="challenge_input" name="challenge_input">
+        <br><br>
+        <hr>
+        <br>
 
-          <br><br>
+        <!-- invite code input -->
+        <label for="code_input">Enter Invite Code:</label>
+        <input type="password" id="code_input" name="code_input">
 
-          <!-- submit button -->
-          <input type="submit" id="submit_button"
-            name="submit_button" value="Sign Up" formaction="signupAction.php" formmethod="get">
+        <br><br>
 
-        </fieldset>
-        
-      </form>
+        <!-- challenge input -->
+        <label id="challenge_label" for="challenge_input">Challenge:
+          <span style="font-weight: normal;">What is the capital of Japan?:</span><sup>*</sup>
+        </label><br>
+        <input type="text" id="challenge_input" name="challenge_input">
 
-      <span style="font-size: 1em"><strong>*: required</strong></span>
+        <br><br>
+
+        <!-- submit button -->
+        <input type="submit" id="submit_button"
+          name="submit_button" value="Sign Up" formaction="signupAction.php" formmethod="get">
+
+      </fieldset>
+      
+    </form>
+
+    <span style="font-size: 1em"><strong>*: required</strong></span>
 
     <?php include "footer.php" ?>
 
